290632a95324386d9fc30d8a9228d7a3f8d418e6
Danilo Castro <danilogcastro@hotmail.com>
2026-05-09 21:08:05 +0200

    initial setup and devise

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::API
+  respond_to :json
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+end
