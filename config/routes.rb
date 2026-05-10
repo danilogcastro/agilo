677d8b102676e944345ecf1c913a88806fd87d36
Danilo Castro <danilogcastro@hotmail.com>
2026-05-10 13:51:01 +0200

    create basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -16,4 +15,33 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # GET /gyms
+  # POST /gyms
+  # GET /gyms/:id
+  # GET /gyms/:gym_id/members
+  # GET /gyms/:gym_id/sessions
+  # GET /gyms/:gym_id/active_users
+  resources :gyms, only: %i[index show create] do
+    resources :sessions, only: %i[index]
+    resources :members, only: %i[index]
+    resources :active_users, only: %i[index]
+  end
+
+  # GET /users/:user_id/memberships
+  # POST /users/:user_id/memberships
+  # DELETE /users/:user_id/memberships
+  # GET /users/:user_id/visits
+  # GET /users/:user_id/visits/:id
+  # POST /users/:user_id/visits
+  # PUT /users/:user_id/visits/:id/check_out
+  resources :users, only: [] do
+    resources :memberships, only: %i[index create destroy]
+    resources :visits, only: %i[index show create] do
+      member do
+        patch :check_out
+        put :check_out
+      end
+    end
+  end
 end
